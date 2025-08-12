ff039cdace4afbe35020d619c6c98dd8f12feee4
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2025-08-12 14:54:03 +0200

    Created PaymentMethodDefaultModel and PaymentMethodAssignedToUserModel

---
+++ app/models/payment_method.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+
+class PaymentMethodDefaultModel(db.Model):
+    __tablename__ = "payment_methods_default"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+class PaymentMethodAssignedToUserModel(db.Model):
+    __tablename__ = "payment_methods_assigned_to_users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
